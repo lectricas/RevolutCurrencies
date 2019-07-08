33851eb617ef7ac3a7125cc35faf2759dc1fe8f6
APolusov <alexey.polusov@firstlinesoftware.com>
2019-07-08 20:33:18 +0300

    clicks now handled through itemTouchListener

--- app/src/main/java/com/lectricas/curriencies/ui/CurrencyContainer.kt
+++ app/src/main/java/com/lectricas/curriencies/ui/CurrencyContainer.kt
@@ -1,33 +1,128 @@
 package com.lectricas.curriencies.ui
 
 import android.content.Context
+import android.graphics.Rect
+import android.text.Editable
+import android.text.TextWatcher
+import android.transition.AutoTransition
+import android.transition.Transition
+import android.transition.TransitionManager
 import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.transition.addListener
+import androidx.core.view.GestureDetectorCompat
+import timber.log.Timber
 
 class CurrencyContainer : LinearLayout {
 
-    private var currentOrder: Int = 0
+    private val textWatcher = Watcher()
 
-    private val DRAW_ORDERS = arrayOf(intArrayOf(0, 1, 2), intArrayOf(2, 1, 0))
+    private val mDetector: GestureDetectorCompat
 
     constructor(context: Context) : super(context) {
-        isChildrenDrawingOrderEnabled = true
+        mDetector = GestureDetectorCompat(context, MyGestureListener())
     }
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        isChildrenDrawingOrderEnabled = true
+        mDetector = GestureDetectorCompat(context, MyGestureListener())
     }
 
     constructor(context: Context, attrs: AttributeSet?, attributeSetId: Int) : super(context, attrs, attributeSetId) {
-        isChildrenDrawingOrderEnabled = true
+        mDetector = GestureDetectorCompat(context, MyGestureListener())
     }
 
-    fun setDrawOrder(order: Int) {
-        currentOrder = order
-        invalidate()
+    private inner class MyGestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onDown(e: MotionEvent?) = true
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            singleTap(e)
+            return true
+        }
     }
 
-    override fun getChildDrawingOrder(childCount: Int, i: Int): Int {
-        return DRAW_ORDERS[currentOrder][i]
+    override fun onInterceptTouchEvent(ev: MotionEvent) = true
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        return mDetector.onTouchEvent(ev)
     }
+
+    fun singleTap(event: MotionEvent) {
+
+        val view = getViewByCoordinates(event.x, event.y) as ViewGroup
+        val auto: Transition = AutoTransition()
+
+        auto.addListener(onEnd = {
+            Timber.d("OnEnd")
+            (parent as ScrollView).smoothScrollTo(0,0)
+            (view.childCount - 1 downTo 1).forEach {
+                (view.getChildAt(it) as AppCompatEditText).requestFocus()
+
+            }
+        })
+
+        TransitionManager.beginDelayedTransition(this, auto)
+
+        removeView(view)
+
+        ((getChildAt(0) as ViewGroup).getChildAt(1) as EditText)
+            .removeTextChangedListener(textWatcher)
+
+        ((view as ViewGroup).getChildAt(1) as EditText)
+            .addTextChangedListener(textWatcher)
+
+        addView(view, 0)
+
+    }
+
+    fun ViewGroup.getViewByCoordinates(x: Float, y: Float): View? {
+        (this.childCount - 1 downTo 0)
+            .map { this.getChildAt(it) }
+            .forEach {
+                val bounds = Rect()
+                it.getHitRect(bounds)
+                if (bounds.contains(x.toInt(), y.toInt())) {
+                    return it
+                }
+            }
+        return null
+    }
+
+    inner class Watcher : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            Timber.d("Changed $s")
+
+            (childCount - 1 downTo 1)
+                .map { getChildAt(it) }
+                .forEach {
+                    ((it as ViewGroup).getChildAt(1) as EditText).setText(s.toString())
+                }
+        }
+    }
+
+//    fun View.showkeyboard() {
+//        viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+//            override fun onGlobalLayout() {
+//                post {
+//                    if (requestFocus()) {
+//                        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//                        imm.showSoftInput(this@showkeyboard, 0)
+//                    }
+//                }
+//                viewTreeObserver.removeOnGlobalLayoutListener(this)
+//            }
+//        })
+//    }
 }
--- app/src/main/java/com/lectricas/curriencies/ui/NewCurrencyActivity.kt
+++ app/src/main/java/com/lectricas/curriencies/ui/NewCurrencyActivity.kt
@@ -4,80 +4,43 @@ import android.graphics.Rect
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.lectricas.curriencies.R
 import kotlinx.android.synthetic.main.activity_new.containerView
+import kotlinx.android.synthetic.main.activity_new.scrollView
 import kotlinx.android.synthetic.main.item_currency.view.currencyId
 import timber.log.Timber
+import android.R.attr.bottom
+import android.R.attr.right
+import android.R.attr.top
+import android.R.attr.left
+import android.widget.LinearLayout.LayoutParams
 
 class NewCurrencyActivity: AppCompatActivity() {
-
-    private val textWatcher = Watcher()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
-        (0..2).forEach {
+        (0..32).forEach {
             val view = layoutInflater.inflate(R.layout.item_currency, null)
             view.currencyId.text = it.toString()
+
+            val params = LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(50, 50, 50, 50)
+            view.layoutParams = params
             containerView.addView(view)
         }
-
-        containerView.setOnTouchListener(object: View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent): Boolean {
-//                val view = containerView.getViewByCoordinates(event.x, event.y)
-//                containerView.removeView(view)
-
-
-//                ((containerView.getChildAt(0) as ViewGroup).getChildAt(1) as EditText)
-//                    .removeTextChangedListener(textWatcher)
-//
-//                ((view as ViewGroup).getChildAt(1) as EditText)
-//                    .addTextChangedListener(textWatcher)
-
-                val view = layoutInflater.inflate(R.layout.item_currency, null)
-                view.currencyId.text = "ADDED"
-
-                containerView.addView(view, 0)
-                view.requestFocus()
-                return false
-            }
-        })
-    }
-
-    inner class Watcher : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            Timber.d("Changed $s")
-
-            (containerView.childCount - 1 downTo 1)
-                .map { containerView.getChildAt(it) }
-                .forEach {
-                    ((it as ViewGroup).getChildAt(1) as EditText).setText(s.toString())
-                }
-
-        }
-    }
-
-    fun ViewGroup.getViewByCoordinates(x: Float, y: Float) : View? {
-        (this.childCount - 1 downTo 0)
-            .map { this.getChildAt(it) }
-            .forEach {
-                val bounds = Rect()
-                it.getHitRect(bounds)
-                if (bounds.contains(x.toInt(), y.toInt())) {
-                    return it
-                }
-            }
-        return null
     }
 }
